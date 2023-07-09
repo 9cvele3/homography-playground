ba3184b590b1fbd76952befac5b30a101cecaff4
Dusan Cvetkovic <9cvele3@gmail.com>
2023-07-09 13:06:53 +0200

    Fixes divergence in ecc

--- src/reg.rs
+++ src/reg.rs
@@ -1,24 +1,28 @@
 // https://docs.rs/arrsac/latest/arrsac/struct.Arrsac.html
 
+use std::ops::Mul;
+
 use imageproc::geometric_transformations::{Projection, warp_into, Interpolation};
 use image::{self};
-use nalgebra::{SMatrix, SVector};
+use nalgebra::{SMatrix, DVector, SVector, DMatrix, Matrix};
 
 #[test]
 fn test_ecc() {
-    let img = image::open("img/lena.jpeg").expect("File not found!").grayscale().into_luma8();
-    let proj = ecc(&img, &img).expect("Registration failed");
+    let img1 = image::open("img/lena-gray.png").expect("File not found!").grayscale().into_luma8();
+    let img2 = image::open("img/lena-trans.png").expect("File not found!").grayscale().into_luma8();
+
+    let proj = ecc(&img1, &img2).expect("Registration failed");
 
 }
 
 const K: usize = 500;
 const N: usize = 9;
-type Jacobian = SMatrix::<f32, K, N>;
-type Params = SVector::<f32, N>;
-type FeatureVector = SVector::<f32, K>;
+type Jacobian = DMatrix::<f32>;//Matrix::<f32, nalgebra::Const<K>, nalgebra::Const<N>, nalgebra::RawStorage<f32, nalgebra::Const<K>, nalgebra::Const<N>>>;
+type Params = DVector::<f32>;//Matrix::<f32, nalgebra::Const<N>, nalgebra::U1, nalgebra::ArrayStorage<f32, N, 1>>;
+type FeatureVector = DVector::<f32>;//SVector::<f32, K>;
 
 fn get_feature_vector(im: &image::ImageBuffer<image::Luma<u8>, Vec<u8>>, coords: &Vec<(u32, u32)>, normalize: bool) -> FeatureVector {
-    let mut fv = FeatureVector::zeros();
+    let mut fv = FeatureVector::zeros(K);
 
     for (i, (x, y)) in coords.iter().enumerate() {
         let pixel = get_pixel_value(im, *x, *y);
@@ -44,8 +48,8 @@ fn ecc(Ir: &image::ImageBuffer<image::Luma<u8>, Vec<u8>>, Iw: &image::ImageBuffe
     let X = get_k_points(K, Ir.width(), Ir.height());
 
     let mut P: Params = Params::from_column_slice(&vec![1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0]);
-    let threshold = 0.1;
-    let max_num_iter = 100;
+    let threshold = 0.01;
+    let max_num_iter = 1000;
     let mut num_iter = 0;
     let ir = get_feature_vector(Ir, &X, true);
 
@@ -54,29 +58,39 @@ fn ecc(Ir: &image::ImageBuffer<image::Luma<u8>, Vec<u8>>, Iw: &image::ImageBuffe
         let mut Imgw: image::ImageBuffer<image::Luma<u8>, Vec<_>> = image::ImageBuffer::new(Ir.width(), Ir.height());
         let h3 = Projection::from_matrix([P[0], P[1], P[2], P[3], P[4], P[5], P[6], P[7], P[8]]).expect("Invalid homography");
         warp_into(&Iw, &h3, Interpolation::Bilinear, [0].into(), &mut Imgw);
+        Imgw.save(format!("img/iter{}.png", num_iter));
         let iw = get_feature_vector(&Imgw, &X, false);
 
-        let G = calculate_jacobian(&Imgw, &X, &P);
-        let GT_G_inv_GT = (G.transpose() * G).pseudo_inverse(0.0001).unwrap() * G.transpose();
-        let PG = G * GT_G_inv_GT;
 
-        let ir_iw = (ir.transpose() * iw)[(0, 0)];
-        let ir_pg_iw = (ir.transpose() * PG * iw)[(0, 0)];
-        let iw_pg_iw = (iw.transpose() * PG * iw)[(0, 0)];
+        let G = calculate_jacobian(&Imgw, &X, &P);
+        let GT = G.transpose();
+        let GT_G = GT.mul(&G);
+        let GT_G_inv_GT = (GT_G).pseudo_inverse(0.00000001).unwrap() * G.transpose();
+        let PG = G * GT_G_inv_GT.clone();
+
+        let ir_iw = (ir.transpose() * iw.clone())[(0, 0)];
+        let ir_pg_iw = (ir.transpose() * PG.clone() * iw.clone())[(0, 0)];
+        let iw_pg_iw = (iw.transpose() * PG.clone() * iw.clone())[(0, 0)];
 
         let inc = if ir_iw > ir_pg_iw {
+            println!("inc1");
             increment1(&ir, &iw, &GT_G_inv_GT, ir_iw, ir_pg_iw, iw_pg_iw)
         } else {
-            let ir_pg_ir = (ir.transpose() * PG * ir)[(0, 0)];
+            println!("inc2");
+            let ir_pg_ir = (ir.transpose() * PG * ir.clone())[(0, 0)];
             increment2(&ir, &iw, &GT_G_inv_GT, ir_iw, iw_pg_iw, ir_pg_ir, ir_pg_iw)
         };
 
         println!("inc.norm {}", inc.norm());
+        //println!("inc: {} {} {} \n {} {} {} \n {} {} {}", inc[0], inc[1], inc[2], inc[3], inc[4], inc[5], inc[6], inc[7], inc[8]);
+
 
         if inc.norm() < threshold {
             return Projection::from_matrix([P[0], P[1], P[2], P[3], P[4], P[5], P[6], P[7], P[8]]);
         } else if num_iter < max_num_iter {
             P += inc;
+            let proj =  Projection::from_matrix([P[0], P[1], P[2], P[3], P[4], P[5], P[6], P[7], P[8]]);
+            //println!("proj: {:?}", proj);
             num_iter += 1;
         } else {
             break;
@@ -120,7 +134,7 @@ fn get_pixel_value(im: &image::ImageBuffer<image::Luma<u8>, Vec<u8>>, x: u32, y:
 
 #[allow(non_snake_case)]
 fn calculate_jacobian(Iw: &image::ImageBuffer<image::Luma<u8>, Vec<u8>>, X: &Vec<(u32, u32)>, P: &Params) -> Jacobian {
-    let mut G = Jacobian::zeros();
+    let mut G = Jacobian::zeros(K, N);
 
     for (k, (x0, x1)) in X.iter().enumerate() {
         let Y = warp_coords(P, (*x0, *x1));
@@ -183,22 +197,22 @@ fn calculate_jacobian(Iw: &image::ImageBuffer<image::Luma<u8>, Vec<u8>>, X: &Vec
         }
         // N = 6
         {
-            let dphi_ofx1_per_dp6 = - numerator1 * P[6] / (denominator * denominator);
-            let dphi_ofx2_per_dp6 = - numerator2 * P[6] / (denominator * denominator);
+            let dphi_ofx1_per_dp6 = - numerator1 * (*x0 as f32) / (denominator * denominator);
+            let dphi_ofx2_per_dp6 = - numerator2 * (*x0 as f32) / (denominator * denominator);
             let el = dI_per_dy1 * dphi_ofx1_per_dp6 + dI_per_dy2 * dphi_ofx2_per_dp6;
             G[(k, 6)] = el;
         }
         // N = 7
         {
-            let dphi_ofx1_per_dp7 = - numerator1 * P[7] / (denominator * denominator);
-            let dphi_ofx2_per_dp7 = - numerator2 * P[7] / (denominator * denominator);
+            let dphi_ofx1_per_dp7 = - numerator1 * (*x1 as f32) / (denominator * denominator);
+            let dphi_ofx2_per_dp7 = - numerator2 * (*x1 as f32) / (denominator * denominator);
             let el = dI_per_dy1 * dphi_ofx1_per_dp7 + dI_per_dy2 * dphi_ofx2_per_dp7;
             G[(k, 7)] = el;
         }
         // N = 8
         {
-            let dphi_ofx1_per_dp8 = - numerator1 * P[8] / (denominator * denominator);
-            let dphi_ofx2_per_dp8 = - numerator2 * P[8] / (denominator * denominator);
+            let dphi_ofx1_per_dp8 = - numerator1 * 1.0 / (denominator * denominator);
+            let dphi_ofx2_per_dp8 = - numerator2 * 1.0 / (denominator * denominator);
             let el = dI_per_dy1 * dphi_ofx1_per_dp8 + dI_per_dy2 * dphi_ofx2_per_dp8;
             G[(k, 8)] = el;
         }
@@ -219,12 +233,13 @@ fn calculate_jacobian(Iw: &image::ImageBuffer<image::Luma<u8>, Vec<u8>>, X: &Vec
 
 
 #[allow(non_snake_case)]
-fn increment1(ir: &FeatureVector, iw: &FeatureVector, GT_G_inv_GT: &SMatrix::<f32, N, K>, ir_iw: f32, ir_pg_iw: f32, iw_pg_iw: f32) -> Params {
-    GT_G_inv_GT * (((iw.norm() * iw.norm() - iw_pg_iw) / (ir_iw - ir_pg_iw))* ir - iw)
+fn increment1(ir: &FeatureVector, iw: &FeatureVector, GT_G_inv_GT: &DMatrix::<f32>, ir_iw: f32, ir_pg_iw: f32, iw_pg_iw: f32) -> Params {
+    GT_G_inv_GT * (((iw.norm() * iw.norm() - iw_pg_iw) / (0.00000000001 + ir_iw - ir_pg_iw))* ir - iw)
 }
 
 #[allow(non_snake_case)]
-fn increment2(ir: &FeatureVector, iw: &FeatureVector, GT_G_inv_GT: &SMatrix::<f32, N, K>, ir_iw: f32, iw_pg_iw: f32, ir_pg_ir: f32, ir_pg_iw: f32) -> Params {
+fn increment2(ir: &FeatureVector, iw: &FeatureVector, GT_G_inv_GT: &DMatrix::<f32>, ir_iw: f32, iw_pg_iw: f32, ir_pg_ir: f32, ir_pg_iw: f32) -> Params {
+    let ir_pg_ir = 0.000000001 + ir_pg_ir;
     let lambda1 = (iw_pg_iw / ir_pg_ir).sqrt();
     let lambda2 = ir_pg_iw - ir_iw / ir_pg_ir;
 
